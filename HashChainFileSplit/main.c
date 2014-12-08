8c125aac55710742483da8d2e8bbd42167c79300
In Chan Hwang <inchanhwang@Ins-MacBook-Pro.local>
2014-12-08 12:16:54 +0900

    Initial Commit

---
+++ HashChainFileSplit/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HashChainFileSplit
+//
+//  Created by In Chan Hwang on 12/8/14.
+//  Copyright (c) 2014 In Chan Hwang. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
